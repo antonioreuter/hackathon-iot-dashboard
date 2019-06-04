0da437f9f0a65ea1b4ff7a6305bca67094df35c9
Jakub Jantosik <jakub.jantosik@philips.com>
2019-06-04 18:50:53 +0200

    Dashboard update

--- scripts/dashboard/components/chartComponent.js
+++ scripts/dashboard/components/chartComponent.js
@@ -5,6 +5,7 @@ class ChartComponent {
     this.init();
     $(window).on('resize', this.setCanvasSize.bind(this));
     this.setCanvasSize();
+    this.createChart();
   }
 
   setCanvasSize() {
@@ -13,8 +14,68 @@ class ChartComponent {
     this.canvas.height(this.container.innerHeight());
   }
 
-  updateCache(cache) {
+  cacheToDataset() {
+    const records = [...this.cache];
+    const data = records.map(record => record.record.stats.statistics.connectedDevices);
+    const labels = records.map(record => record.timestamp);
+    return {
+      data,
+      labels
+    };
+  }
 
+  retrieveChartConfig() {
+    const options = {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Connections'
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Timestamp'
+          }
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Number of connected devices'
+          }
+        }]
+      }
+    };
+    const dataset = this.cacheToDataset();
+    const config = {
+      type: 'line',
+      data: { labels: dataset.labels, datasets: [{ label: 'Timestamp', data: dataset.data }] },
+      options
+    };
+
+    return config;
+  }
+
+  createChart() {
+    this.chart = new Chart(this.ctx, this.retrieveChartConfig());
+  }
+
+  updateChart() {
+    const dataset = this.cacheToDataset();
+    this.chart.data.datasets[0].data = this.cacheToDataset().data;
+    this.chart.data.labels = dataset.labels;
+    this.chart.update();
+  }
+
+  updateCache(cache) {
+    this.cache = cache;
+    this.updateChart();
   }
 
   init() {
